eafc1d36e172d25686d22facd2aec612d99785c2
Pedro Prieto <pedpral@gmail.com>
2018-03-31 01:35:51 +0200

    Locale update

--- config/locales/es_ES.js
+++ config/locales/es_ES.js
@@ -4,5 +4,15 @@
 	"Raíz": "Raíz",
 	"No hay pacientes": "No hay pacientes",
 	"Mensaje": "Mensaje",
+	"Nombre": "Nombre",
+	"Apellidos": "Apellidos",
+	"NIF": "NIF",
+	"Fecha de nacimiento": "Fecha de nacimiento",
+	"Teléfono": "Teléfono",
+	"Dirección": "Dirección",
+	"Email": "Email",
+	"Diagnóstico principal": "Diagnóstico principal",
+	"Observaciones": "Observaciones",
+	"Error": "Error",
 	"Los datos no están en formato CJ": "Los datos no están en formato CJ"
 }
