36046db3f6b9789e4cc38b1366c9e32114c5c90c
Pedro Prieto <pedpral@gmail.com>
2018-03-30 16:16:21 +0200

    Locales update

--- config/locales/es_ES.js
+++ config/locales/es_ES.js
@@ -2,5 +2,7 @@
 	"Pacientes": "Pacientes",
 	"Paciente": "Paciente",
 	"Raíz": "Raíz",
-	"No hay pacientes": "No hay pacientes"
+	"No hay pacientes": "No hay pacientes",
+	"Mensaje": "Mensaje",
+	"Los datos no están en formato CJ": "Los datos no están en formato CJ"
 }
