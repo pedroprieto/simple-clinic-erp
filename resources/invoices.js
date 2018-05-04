6d3e58657b7d116af76175d3d16c91bdfaa597b6
Pedro Prieto <pedpral@gmail.com>
2018-05-04 17:38:40 +0200

    Invoices read only

--- resources/invoices.js
+++ resources/invoices.js
@@ -24,6 +24,7 @@ module.exports = function(router) {
 
 	    // Item data
       item.data = p.invoiceToCJ();
+      item.readOnly = true;
 
 	    // Item href
       item.href = ctx.getLinkCJFormat(router.routesList["invoice"], {invoice: p._id}).href;
